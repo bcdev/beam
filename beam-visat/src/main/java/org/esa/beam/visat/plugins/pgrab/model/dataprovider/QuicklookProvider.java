982492d26811febe766a6067a2e5bd4eab6901e6
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2007-11-09 11:38:23 +0000

    tried to improve performance
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/branches/4.1.x@1337 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-visat/src/main/java/org/esa/beam/visat/plugins/pgrab/model/dataprovider/QuicklookProvider.java
+++ beam-visat/src/main/java/org/esa/beam/visat/plugins/pgrab/model/dataprovider/QuicklookProvider.java
@@ -54,11 +54,8 @@ public class QuicklookProvider implements DataProvider {
 
     public boolean mustCreateData(final RepositoryEntry entry, final Repository repository) {
         final File quickLookFile = getQuickLookFile(repository.getStorageDir(), entry.getProductFile().getName());
-        if (!quickLookFile.exists()) {
-            return true;
+        return !quickLookFile.exists();
         }
-        return false;
-    }
 
     public void createData(final RepositoryEntry entry, final Repository repository) throws IOException {
         final Product product = entry.getProduct();
@@ -273,8 +270,8 @@ public class QuicklookProvider implements DataProvider {
                 return 1;
             }
 
-            final Integer height1 = new Integer(image1.getHeight());
-            final Integer height2 = new Integer(image2.getHeight());
+            final Integer height1 = image1.getHeight();
+            final Integer height2 = image2.getHeight();
 
             return height1.compareTo(height2);
         }
