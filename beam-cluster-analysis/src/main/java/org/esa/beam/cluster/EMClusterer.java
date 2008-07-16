cfe725b8a4f31fb1fc640723099120fd75ff2478
marcop <marcop@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-07-16 08:28:49 +0000

    [BEAM-841] EM clustering does not come to an end for small scenes
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x/branches/4.2.x@2713 d90d2d48-2b33-0410-a973-e72f85796a02

--- beam-cluster-analysis/src/main/java/org/esa/beam/cluster/EMClusterer.java
+++ beam-cluster-analysis/src/main/java/org/esa/beam/cluster/EMClusterer.java
@@ -46,10 +46,11 @@ public class EMClusterer {
      * @param points         the data points.
      * @param clusterCount   the number of clusters.
      * @param iterationCount the number of EM iterations to be made.
+     * @param randomSeed     the seed used to initialize the cluster algorithm
      *
      * @return the cluster decomposition.
      */
-    public static EMClusterSet findClusters(double[][] points, int clusterCount, int iterationCount, int randomSeed) {
+    static EMClusterSet findClusters(double[][] points, int clusterCount, int iterationCount, int randomSeed) {
         return new EMClusterer(points, clusterCount, randomSeed).findClusters(iterationCount);
     }
 
@@ -58,6 +59,7 @@ public class EMClusterer {
      *
      * @param points       the data points.
      * @param clusterCount the number of clusters.
+     * @param randomSeed   the seed used to initialize the cluster algorithm
      */
     public EMClusterer(double[][] points, int clusterCount, int randomSeed) {
         pointCount = points.length;
@@ -145,18 +147,18 @@ public class EMClusterer {
      * Performs an E-step.
      */
     private void stepE() {
+        boolean underflow = false;
         for (int i = 0; i < pointCount; ++i) {
-            double sum = 0.0;
+            // this code is duplicated in EMClusterSet.getPosteriorProbabilities()
             for (int k = 0; k < clusterCount; ++k) {
                 h[k][i] = p[k] * distributions[k].probabilityDensity(points[i]);
-                sum += h[k][i];
-            }
-            if (sum > 0.0) {
-                for (int k = 0; k < h.length; ++k) {
-                    final double t = h[k][i] / sum;
-                    h[k][i] = t;
+                if (h[k][i] == 0.0) {
+                    underflow = true;
+                    break;
                 }
-            } else { // numerical underflow - recompute posterior cluster probabilities
+            }
+            // numerical underflow - compute probabilities using logarithm
+            if (underflow) {
                 final double[] sums = new double[clusterCount];
                 for (int k = 0; k < clusterCount; ++k) {
                     h[k][i] = distributions[k].logProbabilityDensity(points[i]);
@@ -169,9 +171,18 @@ public class EMClusterer {
                     }
                 }
                 for (int k = 0; k < clusterCount; ++k) {
-                    final double t = 1.0 / (1.0 + sums[k]);
-                    h[k][i] = t;
+                    h[k][i] = 1.0 / (1.0 + sums[k]);
                 }
+            }
+            // ensure non-zero probabilities
+             double sum = 0.0;
+            for (int k = 0; k < clusterCount; ++k) {
+                h[k][i] += 1.0E-10;
+                sum += h[k][i];
+            }
+            // normalize probabilities
+            for (int k = 0; k < clusterCount; ++k) {
+                h[k][i] /= sum;
             }
         }
     }
