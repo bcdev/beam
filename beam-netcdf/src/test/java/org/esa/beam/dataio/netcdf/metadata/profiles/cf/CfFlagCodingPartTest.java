ae8d0490541bf1d96592b5cc169fb13289d460c2
marpet <marco.peters@brockmann-consult.de>
2011-10-14 10:05:46 +0200

    [BEAM-1348] Unsigned flag values are not correctly handled if read from NetCDF

--- beam-netcdf/src/test/java/org/esa/beam/dataio/netcdf/metadata/profiles/cf/CfFlagCodingPartTest.java
+++ beam-netcdf/src/test/java/org/esa/beam/dataio/netcdf/metadata/profiles/cf/CfFlagCodingPartTest.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright (C) 2011 Brockmann Consult GmbH (info@brockmann-consult.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
 package org.esa.beam.dataio.netcdf.metadata.profiles.cf;
 
 import junit.framework.TestCase;
 import org.esa.beam.dataio.netcdf.util.DataTypeUtils;
 import org.esa.beam.framework.datamodel.Band;
 import org.esa.beam.framework.datamodel.FlagCoding;
-import org.esa.beam.framework.datamodel.IndexCoding;
 import org.esa.beam.framework.datamodel.MetadataAttribute;
 import org.esa.beam.framework.datamodel.ProductData;
 import ucar.ma2.DataType;
@@ -22,21 +37,16 @@ public class CfFlagCodingPartTest extends TestCase {
         FlagCoding flagCoding = new FlagCoding("some_flags");
         flagBand.setSampleCoding(flagCoding);
         flagCoding.setDescription("A Flag Coding");
-        MetadataAttribute attribute;
-
-        attribute = new MetadataAttribute("FIRST_FLAG", ProductData.TYPE_UINT8);
-        attribute.getData().setElemInt(1);
-        flagCoding.addAttribute(attribute);
-
-        attribute = new MetadataAttribute("SECOND_FLAG", ProductData.TYPE_UINT8);
-        attribute.getData().setElemInt(2);
-        flagCoding.addAttribute(attribute);
+        for (int i = 0; i < 8; i++) {
+            addFlag(flagCoding, i);
+        }
 
         NetcdfFileWriteable writeable = NetcdfFileWriteable.createNew("not stored");
         writeable.addDimension("y", flagBand.getSceneRasterHeight());
         writeable.addDimension("x", flagBand.getSceneRasterWidth());
         final DataType ncDataType = DataTypeUtils.getNetcdfDataType(flagBand.getDataType());
-        CfBandPart.writeCfBandAttributes(flagBand, writeable.addVariable(flagBand.getName(), ncDataType, writeable.getRootGroup().getDimensions()));
+        CfBandPart.writeCfBandAttributes(flagBand, writeable.addVariable(flagBand.getName(), ncDataType,
+                                                                         writeable.getRootGroup().getDimensions()));
         CfFlagCodingPart.writeFlagCoding(flagBand, writeable);
 
         Variable someFlagsVariable = writeable.findVariable("flag_band");
@@ -44,10 +54,23 @@ public class CfFlagCodingPartTest extends TestCase {
         Attribute flagMasksAttrib = someFlagsVariable.findAttribute("flag_masks");
         assertNotNull(flagMasksAttrib);
         assertEquals(someFlagsVariable.getDataType(), flagMasksAttrib.getDataType());
-        assertEquals(2, flagMasksAttrib.getLength());
+        assertEquals(8, flagMasksAttrib.getLength());
+        assertTrue(flagMasksAttrib.isUnsigned());
+        for (int i = 0; i < 8; i++) {
+            assertEquals(1 << i, flagMasksAttrib.getValues().getInt(i));
+        }
 
         Attribute descriptionAttrib = someFlagsVariable.findAttribute("long_name");
         assertNotNull(flagCoding.getDescription(), descriptionAttrib.getStringValue());
 
+    }
+
+    private void addFlag(FlagCoding flagCoding, int index) {
+        MetadataAttribute attribute;
+
+        attribute = new MetadataAttribute(String.format("%d_FLAG", index + 1), ProductData.TYPE_UINT8);
+        final int maskValue = 1 << index;
+        attribute.getData().setElemInt(maskValue);
+        flagCoding.addAttribute(attribute);
     }
 }
