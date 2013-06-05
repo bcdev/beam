bd89c71df4c8e85170565b246fcb7f6a573943a6
marpet <marco.peters@brockmann-consult.de>
2013-06-05 17:56:13 +0200

    [BEAM-1495] NaN should be considered in L3-Binning (cherry picked from commit 4a27a6616efcdbf5100baf4a6a8d61775994935e)
    Conflicts:
    	beam-binning2/pom.xml
    	beam-binning2/src/main/java/org/esa/beam/binning/aggregators/AggregatorAverage.java
    	beam-binning2/src/main/java/org/esa/beam/binning/aggregators/AggregatorPercentile.java
    	beam-binning2/src/main/resources/module.xml

--- beam-binning2/src/main/java/org/esa/beam/binning/aggregators/AggregatorPercentile.java
+++ beam-binning2/src/main/java/org/esa/beam/binning/aggregators/AggregatorPercentile.java
@@ -1,21 +1,5 @@
 /*
- * Copyright (C) 2010 Brockmann Consult GmbH (info@brockmann-consult.de)
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option)
- * any later version.
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, see http://www.gnu.org/licenses/
- */
-
-/*
- * Copyright (C) 2012 Brockmann Consult GmbH (info@brockmann-consult.de)
+ * Copyright (C) 2013 Brockmann Consult GmbH (info@brockmann-consult.de)
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -58,6 +42,7 @@ public class AggregatorPercentile extends AbstractAggregator {
     private final int varIndex;
     private final int percentage;
     private final String mlName;
+    private final String icName;
 
     public AggregatorPercentile(VariableContext varCtx, String varName, Integer percentage) {
         this(getVarIndex(varCtx, varName), varName, getEffectivePercentage(percentage));
@@ -78,22 +63,36 @@ public class AggregatorPercentile extends AbstractAggregator {
         this.varIndex = varIndex;
         this.percentage = percentage;
         this.mlName = "ml." + varName;
+        this.icName = "ic." + varName;
     }
 
     @Override
     public void initSpatial(BinContext ctx, WritableVector vector) {
         vector.set(0, 0.0f);
+        ctx.put(icName, new int[1]);
     }
 
     @Override
     public void aggregateSpatial(BinContext ctx, Observation observationVector, WritableVector spatialVector) {
-        final float value = observationVector.get(varIndex);
-        spatialVector.set(0, spatialVector.get(0) + value);
+        float value = observationVector.get(varIndex);
+        if (!Float.isNaN(value)) {
+            spatialVector.set(0, spatialVector.get(0) + value);
+        } else {
+            // We count invalids rather than valid because it is more efficient.
+            // (Key/value map operations are relatively slow, and it is more likely that we will receive valid measurements.)
+            ((int[])ctx.get(icName))[0]++;
+        }
     }
 
     @Override
     public void completeSpatial(BinContext ctx, int numSpatialObs, WritableVector spatialVector) {
-        spatialVector.set(0, spatialVector.get(0) / numSpatialObs);
+        Integer invalidCount = ((int[])ctx.get(icName))[0];
+        int effectiveCount =  numSpatialObs - invalidCount;
+        if (effectiveCount > 0) {
+            spatialVector.set(0, spatialVector.get(0) / effectiveCount);
+        }else {
+            spatialVector.set(0, Float.NaN);
+        }
     }
 
     @Override
@@ -105,7 +104,10 @@ public class AggregatorPercentile extends AbstractAggregator {
     @Override
     public void aggregateTemporal(BinContext ctx, Vector spatialVector, int numSpatialObs, WritableVector temporalVector) {
         GrowableVector measurementsVec = ctx.get(mlName);
-        measurementsVec.add(spatialVector.get(0));
+        float value = spatialVector.get(0);
+        if (!Float.isNaN(value)) {
+            measurementsVec.add(value);
+        }
     }
 
     @Override
@@ -119,7 +121,13 @@ public class AggregatorPercentile extends AbstractAggregator {
 
     @Override
     public void computeOutput(Vector temporalVector, WritableVector outputVector) {
-        outputVector.set(0, temporalVector.get(0));
+        float value = temporalVector.get(0);
+        if (!Float.isNaN(value)) {
+            outputVector.set(0, value);
+        }else {
+            // todo - use fillValue here (nf)
+            outputVector.set(0, Float.NaN);
+        }
     }
 
     @Override
@@ -141,6 +149,7 @@ public class AggregatorPercentile extends AbstractAggregator {
      *
      * @param p            The percentage in percent ranging from 0 to 100.
      * @param measurements Sorted array of measurements.
+     *
      * @return The  p-th percentile.
      */
     public static float computePercentile(int p, float[] measurements) {
@@ -159,31 +168,32 @@ public class AggregatorPercentile extends AbstractAggregator {
         return yp;
     }
 
-    public static class Config extends AggregatorConfig {
-        @Parameter
-        String varName;
-        @Parameter
-        Integer percentage;
-        @Parameter
-        Float fillValue;
+public static class Config extends AggregatorConfig {
 
-        public Config() {
-            super(Descriptor.NAME);
-        }
+    @Parameter
+    String varName;
+    @Parameter
+    Integer percentage;
+    @Parameter
+    Float fillValue;
 
-        public void setVarName(String varName) {
-            this.varName = varName;
-        }
-
-        public void setPercentage(Integer percentage) {
-            this.percentage = percentage;
-        }
-
-        @Override
-        public String[] getVarNames() {
-            return new String[] {varName};
-        }
+    public Config() {
+        super(Descriptor.NAME);
     }
+
+    public void setVarName(String varName) {
+        this.varName = varName;
+    }
+
+    public void setPercentage(Integer percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public String[] getVarNames() {
+        return new String[]{varName};
+    }
+}
 
 
     private static int getVarIndex(VariableContext varCtx, String varName) {
@@ -198,26 +208,26 @@ public class AggregatorPercentile extends AbstractAggregator {
         return (percentage != null ? percentage : 90);
     }
 
-    public static class Descriptor implements AggregatorDescriptor {
+public static class Descriptor implements AggregatorDescriptor {
 
-        public static final String NAME = "PERCENTILE";
+    public static final String NAME = "PERCENTILE";
 
-        @Override
-        public String getName() {
-            return NAME;
-        }
-
-        @Override
-        public AggregatorConfig createConfig() {
-            return new Config();
-        }
-
-        @Override
-        public Aggregator createAggregator(VariableContext varCtx, AggregatorConfig aggregatorConfig) {
-            PropertySet propertySet = aggregatorConfig.asPropertySet();
-            return new AggregatorPercentile(varCtx,
-                                            (String)propertySet.getValue("varName"),
-                                            (Integer) propertySet.getValue("percentage"));
-        }
+    @Override
+    public String getName() {
+        return NAME;
     }
+
+    @Override
+    public AggregatorConfig createConfig() {
+        return new Config();
+    }
+
+    @Override
+    public Aggregator createAggregator(VariableContext varCtx, AggregatorConfig aggregatorConfig) {
+        PropertySet propertySet = aggregatorConfig.asPropertySet();
+        return new AggregatorPercentile(varCtx,
+                                        (String) propertySet.getValue("varName"),
+                                        (Integer) propertySet.getValue("percentage"));
+    }
+}
 }
